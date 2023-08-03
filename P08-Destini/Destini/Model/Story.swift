b1230bf2c84433782ee6458f6772527bae07ef06
Evgenii <mazrukho@gmail.com>
2023-08-03 04:02:17 +0300

    add files P08-Destini

---
+++ P08-Destini/Destini/Model/Story.swift
@@ -0,0 +1,8 @@
+//
+//  Story.swift
+//  Destini
+//
+//  Created by Evgenii Mazrukho on 03.08.2023.
+//
+
+import Foundation
---
+++ P08-Destini/Destini/Model/StoryBrain.swift
@@ -0,0 +1,8 @@
+//
+//  StoryBrain.swift
+//  Destini
+//
+//  Created by Evgenii Mazrukho on 03.08.2023.
+//
+
+import Foundation
