0baeb03fc7d4af734e35ed0a8e13827d84227b64
Evgenii Mazrukho <mazrukho@gmail.com>
2023-08-05 18:27:32 +0300

    add test logic P09-BMICalculator

--- P09-BMICalculator/BMICalculator/Controller/ViewController.swift
+++ P09-BMICalculator/BMICalculator/Controller/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+//        print(String(format: "%.2f", sender.value))
+        heightLabel.text = String(format: "%.2fm", sender.value)
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+//        print(String(format: "%.0f", sender.value))
+        weightLabel.text = "\(Int(sender.value))Kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight / (pow(height, 2))
+        print(String(format: "%.2f", bmi))
+    }
 }
-
---
+++ P09-BMICalculator/BMICalculator/Views/CalculateView.swift
@@ -0,0 +1,18 @@
+//
+//  CalculateView.swift
+//  BMICalculator
+//
+//  Created by Evgenii Mazrukho on 05.08.2023.
+//
+
+import UIKit
+
+class CalculateView: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
