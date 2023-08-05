3da20f3f47ee7e2738a4051f46f13d3bb1e7b69f
Evgenii Mazrukho <mazrukho@gmail.com>
2023-08-05 21:24:49 +0300

    applying the MVC and finishing up with logic

---
+++ P09-BMICalculator/BMICalculator/Model/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMICalculator
+//
+//  Created by Evgenii Mazrukho on 05.08.2023.
+//
+
+import UIKit
+
+struct BMI {
+    
+    let value: Float
+    let advice: String
+    let color: UIColor
+    
+}
