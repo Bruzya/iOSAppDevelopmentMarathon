e8b1a791610e49cd5065bf0aa473bb65ac295977
Evgenii Mazrukho <mazrukho@gmail.com>
2023-08-26 23:31:55 +0300

    add Model, JSON Decoder

---
+++ P11-Clima/Clima/Model/WeatherData.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Evgenii Mazrukho on 26.08.2023.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    
+    let name: String
+    let main: Main
+    let weather: [Weather]
+    
+}
+
+struct Main: Decodable {
+    
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    
+    let description: String
+    
+}
