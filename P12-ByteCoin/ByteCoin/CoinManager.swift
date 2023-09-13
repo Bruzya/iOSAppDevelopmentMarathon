70b06a26b9a16d7e0e40bf6999e91911e3d5ceec
Evgenii Mazrukho <mazrukho@gmail.com>
2023-09-13 15:21:06 +0300

    add files P12-ByteCoin

---
+++ P12-ByteCoin/ByteCoin/CoinManager.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Evgenii Mazrukho on 13.09.2023.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    
+}
