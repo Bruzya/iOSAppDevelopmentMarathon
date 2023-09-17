a54e4ba5751afca1cac9af2cce7ea24b6e118509
Evgenii Mazrukho <mazrukho@gmail.com>
2023-09-17 17:52:51 +0300

    add Firebase auth, createUser

--- P13-FlashChat/FlashChat/Controllers/Authentication/RegisterViewController.swift
+++ P13-FlashChat/FlashChat/Controllers/Authentication/RegisterViewController.swift
@@ -6,13 +6,22 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let error {
+                    print(error)
+                } else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
-    
 }
