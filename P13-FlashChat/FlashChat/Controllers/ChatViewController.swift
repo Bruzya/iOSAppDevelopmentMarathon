0e8e476d82430828e08ef3e6aef8181062f25ed8
Evgenii Mazrukho <mazrukho@gmail.com>
2023-09-21 16:03:55 +0300

    add Model and TableView config

--- P13-FlashChat/FlashChat/Controllers/ChatViewController.swift
+++ P13-FlashChat/FlashChat/Controllers/ChatViewController.swift
@@ -14,9 +14,18 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    //MARK: - Properties
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey!"),
+        Message(sender: "a@b.com", body: "Hello!"),
+        Message(sender: "1@2.com", body: "What's up?")
+    ]
+    
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
     }
@@ -37,3 +46,26 @@ class ChatViewController: UIViewController {
         }
     }
 }
+
+//MARK: - UITableViewDataSource
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        //if iOS 14 and above
+        if #available(iOS 14.0, *) {
+            var content = cell.defaultContentConfiguration()
+            content.text = messages[indexPath.row].body
+            cell.contentConfiguration = content
+            return cell
+        } else {
+            //Fallback on earlier versions beacause of cell.textlabel will be deprecated in the future
+            cell.textLabel?.text = messages[indexPath.row].body
+            return cell
+        }
+    }
+}
+
