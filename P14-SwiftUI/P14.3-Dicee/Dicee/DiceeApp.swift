953f7679137f276db578f7041358f5ab195dd06c
Evgenii Mazrukho <mazrukho@gmail.com>
2023-09-25 16:19:00 +0300

    add files P14.3 Dicee

---
+++ P14-SwiftUI/P14.3-Dicee/Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by Evgenii Mazrukho on 25.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
