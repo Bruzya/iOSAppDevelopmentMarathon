e56c6b018ce97f5303498e7d3dbc34ff20857f73
Evgenii Mazrukho <mazrukho@gmail.com>
2023-09-26 22:46:42 +0300

    add files P14.4-H4XORNews

---
+++ P14-SwiftUI/P14.4-H4XORNews/H4XOR News/H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NewsApp.swift
+//  H4XOR News
+//
+//  Created by Evgenii Mazrukho on 26.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
